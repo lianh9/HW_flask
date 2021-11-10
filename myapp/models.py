5e387b5e9572fb0b8440aa61847f5b0e01a52451
Lian Huang <lian.huang@sjsu.edu>
2021-11-09 21:58:52 -0800

    Fixed duplicates error

--- myapp/models.py
+++ myapp/models.py
@@ -2,8 +2,9 @@ from enum import unique
 from myapp import db
 
 class TopCities(db.Model):
-    city_name = db.Column(db.String(64), primary_key=True, nullable = False)
-    city_rank = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    city_name = db.Column(db.String(64),nullable = False)
+    city_rank = db.Column(db.Integer,nullable = False)
     is_visited = db.Column(db.Boolean)
 
     def __repr__(self):
